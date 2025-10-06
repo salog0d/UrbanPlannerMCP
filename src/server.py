5fb4037cdc7c30d8e6ca4b9ec677755fef377276
Salog0d <david.salomon.nava11@gmail.com>
2025-10-06 12:13:56 -0600

    fix(imports): changed imports from other modules

--- src/server.py
+++ src/server.py
@@ -1,24 +1,21 @@
+from __future__ import annotations
 from mcp.server.fastmcp import FastMCP
-from schemas.poulation_schema import (
+from schemas.population_schema import (
     PopulationRequest,
     PopulationResponse
 )
 
-from __future__ import annotations
-
-from src.schemas.inequality_indicators_schema import (
+from schemas.inequality_indicators_schema import (
     UnequalityIndicatorsRequest,
     UnequalityIndicatorsResponse,
 )
 
-from src.core.settings import get_settings
+from core.settings import get_settings
 
-from src.tools.city_infra_model import InfaModelEndpoints
-from src.tools.population_inequalities_model import InequalityModel
+from tools.city_infra_model import InfaModelEndpoints
+from tools.population_inequalities_model import InequalityModel
 
-from src.prompts import _prompts as _prompts  
-from src.resources import _resources as _resources  
-
+from prompts import _prompts as _prompts   
 settings = get_settings()
 mcp = FastMCP(settings.app_name, stateless_http=settings.stateless_http)
 
